b0b1d794abb59f1687e6aa6644eeb8a05fe4ae21
Roronoa-17 <111036213+Roronoa-17@users.noreply.github.com>
2024-06-28 23:25:42 +0530

    multimap and unordered map

---
+++ STL_DataStructures/MultiMap&UnorderedMap.cpp
@@ -0,0 +1,10 @@
+void explainMultiMap()
+{
+    // everything same as map, only it can store multiple keys
+    // only mpp[key] canot be used here
+}
+
+void explainUnorderedMap()
+{
+    // same as set 
+}

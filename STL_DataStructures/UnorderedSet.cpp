be82a57b382467ccce606783ed8b8c4a6e5c1763
Roronoa-17 <thepriyesh17@gmail.com>
2024-07-02 23:33:35 +0530

    unordered set

---
+++ STL_DataStructures/UnorderedSet.cpp
@@ -0,0 +1,11 @@
+
+void explainUset()
+{
+    unordered_set<int> st;
+
+    /*
+    lower_bound and upper_bound function does not works, rest all functions are same.
+    it does not stores in any particular order it has a better complexity than set in most cases
+    , except some when collision happens
+    */
+}

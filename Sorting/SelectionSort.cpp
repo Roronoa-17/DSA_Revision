e215cb2fffb0fced96a3b1bc655d50d762c268af
Roronoa-17 <thepriyesh17@gmail.com>
2024-08-02 22:49:48 +0530

    Selection sort

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+    for(int i = 0; i <= n-2 ; i++)
+    {
+        int min = i;
+        for(int j=i; j <= n-1; j++)
+        {
+            if(arr[j] < arr[i]) min = j;
+        }
+        int t = arr[min];
+        arr[min] = arr[i];
+        arr[i] = t;
+    }
+}
+
+int main()
+{
+    int n ;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++) cin>> arr[i];
+
+    selection_sort(arr, n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+} 

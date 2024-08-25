9d1978da701c5a7f15b9e9415df1f3f8e21fa1b3
Priyesh H. Gawali <thepriyesh17@gmail.com>
2024-08-25 23:19:30 +0530

    oops in python

---
+++ Python/class.py
@@ -0,0 +1,17 @@
+class Person:
+    name = "Harry"
+    occupation = "Software Developer"
+    networth = 10
+    def info(self):
+        print(f"{self.name} is a {self.occupation}")
+
+
+a = Person()
+b = Person()
+a.name = "Shubham"
+a.occupation = "Accountant"
+
+b.name = "Nikita"
+b.occupation = "HR"
+a.info()
+b.info()
